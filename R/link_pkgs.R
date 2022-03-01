29c213a75c83244e898656941b5aa72238c819b3
be-marc <marcbecker@posteo.de>
2022-03-01 15:05:29 +0100

    feat: add overview of objects

--- R/link_pkgs.R
+++ R/link_pkgs.R
@@ -11,11 +11,10 @@ link_pkgs = function(x, remove = character()) {
   checkmate::assert_list(x, "character")
   cran_pkgs = intersect(rownames(available.packages()), unlist(x, use.names = FALSE))
 
-  mlr3misc::map_chr(x, function(pkgs) {
+  mlr3misc::map(x, function(pkgs) {
     pkgs = setdiff(pkgs, remove)
     ii = pkgs %in% cran_pkgs
     pkgs[ii] = mlr3misc::map_chr(pkgs[ii], function(x) as.character(htmltools::a(href = sprintf("https://cran.r-project.org/package=%s", x), x)))
-
-    paste0(pkgs, collapse = ", ")
+    pkgs
   })
 }
