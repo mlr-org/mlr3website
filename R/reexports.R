127b8392402439163cafe93cac72c91e44bd4033
be-marc <marcbecker@posteo.de>
2022-11-17 10:28:35 +0100

    fix: ref_pkg

--- R/reexports.R
+++ R/reexports.R
@@ -1,15 +1,7 @@
-#' @importFrom mlr3book mlr_pkg
+#' @importFrom mlr3book ref_pkg
 #' @export
-mlr3book::mlr_pkg
+mlr3book::ref_pkg
 
 #' @importFrom mlr3book ref
 #' @export
 mlr3book::ref
-
-#' @importFrom mlr3book cran_pkg
-#' @export
-mlr3book::cran_pkg
-
-#' @importFrom mlr3book gh_pkg
-#' @export
-mlr3book::gh_pkg
